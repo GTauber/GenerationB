1fedf7793868a57e337e0160ae84393e749be14e
Gabriel Tauber <gabri.tauber@gmail.com>
2020-11-12 16:13:44 -0300

    Exs Corrigido

---
+++ Java/Eclipse/12-11-2020/src/ex2/Ex2.java
@@ -0,0 +1,18 @@
+package ex2;
+
+public class Ex2 {
+	public static void main(String args[])
+	{
+		String About = ("Olá, me chamo Gabriel Tauber e venho dizer quais habilidades e mentalidades tenho utilizado com mais frequencia\n"
+				+ "até agora, e são elas, entre as habilidades: orientação aos detalhes, gerenciamento de tempo e proatividade.\n"
+				+ "Escolhi elas por que percebi que sem gerenciar bem meu tempo, não conseguiria manter atenção aos detalhes, que são de extrema\n"
+				+ "importancia nos códigos! Utilizo a proatividade para sempre criar e tentar ir além do que foi pedido.\n"
+				+ "E as mentalidades que eu tenho utilizado com mais frequencia são: Mentalidade do crescimento e persistência\n"
+				+ "Utilizo a mentalidade do crescimento sempre para pensar que minha curva de aprendizado não é diferente da curva dos meus\n"
+				+ "colegas e isso não pode me desanimar e a persistencia para não desistir ao encontrar desafios.!!");
+		System.out.println(About);
+		
+	}
+
+
+}
