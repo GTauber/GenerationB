62ec0ed87837f29169bc9e12cd82072716454dc4
Gabriel Tauber <gabri.tauber@gmail.com>
2020-11-12 15:50:04 -0300

    Java ex 1

---
+++ Java/Eclipse/12-11-2020 Ex1/src/ex1/Ex1.java
@@ -0,0 +1,15 @@
+package ex1;
+
+public class Ex1 {
+	public static void main(String args[])
+	{
+		String About = ("Olá, me chamo Gabriel Tauber, tenho 19 anos e sou aspirante a Dev Java Jr.\n Tive meu primeiro contato com a programação ainda muito cedo"
+				+ "quando eu tinha uns 10 anos, mas desde então nunca mais mexi com isso, agora resolvi voltar meu foco para aquilo que me "
+				+ "atraiu ainda quando eu era pequeno, meu objetivo aqui é conseguir uma introdução no mundo da programação, trabalhar e se Deus "
+				+ "quiser pagar minha faculdade de engenharia da computação.\n"
+				+ "Amo tudo que voa, passaros, aviões, drones, pra mim se eu tivesse condições com certeza aeromodelismo seria meu hobbie.");
+		System.out.println(About);
+		
+	}
+
+}
