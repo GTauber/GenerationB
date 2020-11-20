4d7d65bead092125f1d3b2a50a365de28cd74c7f
Gabriel Tauber <gabri.tauber@gmail.com>
2020-11-20 02:00:41 -0300

    Ex3 OOP

---
+++ Java/Eclipse/19-11 Lista 1-3/src/Ex3/Ex3OOP.java
@@ -0,0 +1,49 @@
+package Ex3;
+
+public class Ex3OOP {
+	public static void main (String args[])
+	{
+		//Meaning:
+		System.out.println("Car: A machine to move around, a mode of transport in earth.");
+		//Does:
+		System.out.println("Nothing without one human being");
+		//Substantive:
+		System.out.println("Car, machine");
+		//Verbs:
+		System.out.println("Move, transport.");
+		//Meaning:
+		System.out.println("People: A human being, have life, walk, talk and are able to think");
+		//Does:
+		System.out.println("Walk, think, jump, study etc...");
+		//Substantive:
+		System.out.println("Human");
+		//Verbs:
+		System.out.println("Have, walk, talk...");
+		//Meaning:
+		System.out.println("House: a place to fell safe, sleep, watch tv...");
+		//Does:
+		System.out.println("Nothing without one human being.");
+		//Substantive:
+		System.out.println("place, tv");
+		//Verbs:
+		System.out.println("fell, sleep");
+		
+		//Object 1:
+				System.out.println("Car: Attributes: Color, horsepower, Powered, Sedan.\n"
+								 + "     Behavior: Aceleration, brake, moveBackwards ");
+		//Object 2:
+				System.out.println("Human: Weight, height, haircolor,beautifull,gender.\n"
+								 + "     Behavior: Smiles, jumps, see, Study, Walk, Run.");
+		//Object 3:
+				System.out.println("TV: Attributes: RGB, LED, Pol, Brand.\n"
+								 + "     Behavior: PowerOn/Off, Bright, Changes.");
+		//Object 4:
+				System.out.println("Machine: Strenght: Kind.\n"
+								 + "     Behavior: EnergyConverter, Emitter, Produces Something.");
+		//How they all interact with each other?
+				System.out.println("All of they are dependent of a human being, a human picks one car, goes back to his house and lay\n"
+						+ "down to watch TV, after a little while he gets up, go to the kitchen and pick any machine there\n"
+						+ "to do something to eat.");
+	}
+
+}
